5a82cdd70a36cf24de52e6ab682728370d498054
Gustavo Bethelmy <tenchu96@gmail.com>
2019-01-27 05:34:08 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(cors())
+
+app.set('port',process.env.PORT || 3000)
+
+app.get('/',(req,res)=>{
+    res.json({status: 'online'})
+})
+
+app.get('/whoami',(req,res)=>{
+    res.json({
+        ipaddress: req.ip,
+        language: req.acceptsLanguages(),
+        software: req.headers["user-agent"]
+    })
+})
+
+app.listen(app.get('port'),()=>{
+    console.log(`server listening on http://localhost:${app.get('port')}`)
+})
